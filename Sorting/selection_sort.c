345c96962307c3d3a0005fcdb0cb016b615a2fdb
Shivam Chauhan <chauhanshivam999@gmail.com>
2019-02-02 00:08:05 +0530

    Implemented Selection Sort

---
+++ Sorting/selection_sort.c
@@ -0,0 +1,55 @@
+/*
+   Author : Shivam Chauhan
+   Date   : Feb 1 , 2019
+   Implementation of Selection Sorting
+*/
+#include <stdio.h>
+#include <stdlib.h>
+void selection_sort( int *arr);
+void display( int *arr);
+int size ;
+int main()
+{
+	int i ;
+	printf("\nEnter the size of the array ");
+	scanf("%d",&size);
+	int *arr = (int *)malloc(sizeof(int));
+	for ( i = 0 ; i < size ; i++ )
+	{
+		scanf("%d",&arr[i]);
+	}
+	selection_sort(arr);
+	display(arr);
+}
+void selection_sort( int *arr)
+{
+	int i , j , min , tmp ;
+	for ( i = 0 ; i < size - 1 ; i++ )
+	{
+		min = i;
+		for ( j = i + 1 ; j < size ; j++ )
+		{
+			if ( arr[min] > arr [j] ) 
+			{
+				min = j ;
+			}
+		}
+		if ( i != min )
+		{
+			tmp = arr[min];
+			arr[min] = arr[i];
+			arr[i] = tmp ;
+		}
+	}
+}
+void display( int *arr)
+{
+	int i ;
+	printf("\nSorted Elements are : \n");
+	for ( i = 0 ; i < size ; i++ )
+	{
+		printf("%d ",arr[i]);
+	}
+}
+
+
