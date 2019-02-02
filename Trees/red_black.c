f465282a2051bb5c919c68e383fc37a4a0d410fe
Shivam Chauhan <chauhanshivam999@gmail.com>
2019-02-03 02:12:40 +0530

    Implemented Deletion in Red Black Trees Successfully:-)

--- Trees/red_black.c
+++ Trees/red_black.c
@@ -26,9 +26,23 @@ void right_rotate( struct red_black *ptr );
 void insert(int data );
 void insert_balance( struct red_black *ptr );
 void display_tree( struct red_black *ptr , int level );
+void preorder_display( struct red_black *root );
+void inorder_display( struct red_black *root );
+void postorder_display( struct red_black *root );
+int get_height( struct red_black *ptr );
+struct red_black *search( int data );
+void delete( int data );
+void delete_balance( struct red_black *ptr );
+struct red_black *get_successor( struct red_black *ptr);
+struct red_black *leaf ;
 int main()
 {
 	int choice , element ;
+  leaf = ( struct red_black *)malloc(sizeof(struct red_black));
+  leaf -> info = -1;
+  leaf -> color = BLACK ;
+  leaf -> parent = leaf -> left_child = leaf -> right_child = NULL;
+  root = leaf ;
 	while(1)
   	{
             //Resetting the color
@@ -42,9 +56,10 @@ int main()
             printf("\n\t\t\t\t 4.  Inorder ") ;
             printf("\n\t\t\t\t 5.  Postorder ") ;
             printf("\n\t\t\t\t 6.  Count ") ; 
-            printf("\n\t\t\t\t 7.  Height ") ;          
-            printf("\n\t\t\t\t 8.  Delete") ;                   
-            printf("\n\t\t\t\t 9.  Exit ");
+            printf("\n\t\t\t\t 7.  Height ") ;
+            printf("\n\t\t\t\t 8.  Search ") ;                    
+            printf("\n\t\t\t\t 9.  Delete") ;                   
+            printf("\n\t\t\t\t10.  Exit ");
             printf("\n\t\t\t\t\nEnter your Choice : ") ;
             scanf("%d",&choice);	
             switch(choice)
@@ -64,7 +79,7 @@ int main()
                            printf("\nTree is Empty!!!");
                            break;
 
-	      /*          case 3 : if (!is_empty())
+	                case 3 : if (!is_empty())
       			               {
       			                 printf("\nThe Preorder is : \n");
 		                         preorder_display( root );
@@ -101,14 +116,26 @@ int main()
 
                   case 8 : if (!is_empty())
                            {
-                              printf("\nEnter the element to be deleted :");
+                              printf("\nEnter the element to be searched :");
                               scanf("%d",&element); 
-                              root = delete( root , element );
+                              if ( search(element )!= leaf )
+                                  printf("\n%d is present in the tree.",element);
+                              else
+                                  printf("\n%d is not present in the tree.",element);
                            }
                            else
                               printf("\nTree is Empty!!!");  
 
-          */                 break ;
+                           break ;
+                  case 9 : if (!is_empty())
+                           {
+                              printf("\nEnter the element to be deleted :");
+                              scanf("%d",&element); 
+                              delete(element);
+                           }
+                           else
+                              printf("\nTree is Empty!!!");  
+                           break ;
 
         	     default : exit(0);
       			   break;
@@ -119,19 +146,19 @@ int main()
 }
 int is_empty()
 {
-    return root == NULL ;
+    return root == leaf ;
 }
 
 void left_rotate( struct red_black *ptr)
 {
     struct red_black *child_ptr = ptr -> right_child ;
     ptr -> right_child = child_ptr -> left_child ;
-    if ( child_ptr -> left_child != NULL )
+    if ( child_ptr -> left_child != leaf )
     {
         child_ptr -> left_child -> parent = ptr ;
     }
     child_ptr -> parent = ptr -> parent ;
-    if ( ptr -> parent == NULL )
+    if ( ptr -> parent == leaf )
     {
         root = child_ptr ;
     }
@@ -151,12 +178,12 @@ void right_rotate( struct red_black *ptr)
 {
     struct red_black *child_ptr = ptr -> left_child ;
     ptr -> left_child = child_ptr -> right_child ;
-    if ( child_ptr -> right_child != NULL )
+    if ( child_ptr -> right_child != leaf )
     {
         child_ptr -> right_child -> parent = ptr ;
     }
     child_ptr -> parent = ptr -> parent ;
-    if ( ptr -> parent == NULL )
+    if ( ptr -> parent == leaf )
     {
         root = child_ptr ;
     }
@@ -175,9 +202,9 @@ void right_rotate( struct red_black *ptr)
 void insert(int data)
 {
     struct red_black *tmp, *ptr, *par;
-    par = NULL;
+    par = leaf;
     ptr = root;
-    while( ptr != NULL )
+    while( ptr != leaf )
     {
         par = ptr;
         if( data < ptr -> info )
@@ -195,23 +222,24 @@ void insert(int data)
         }
     }
     tmp = (struct red_black *) malloc(sizeof(struct red_black));
-    tmp->info = data;
-    tmp->left_child = NULL;
-    tmp->right_child = NULL;
-    tmp->color = RED;
-    tmp->parent = par;
+    tmp -> info = data;
+    tmp -> left_child = leaf;
+    tmp -> right_child = leaf;
+    tmp -> color = RED;
+    tmp -> parent = par;
+    size++;
 
-    if(par==NULL)
+    if( par == leaf )
     {
       root = tmp;
     }
-    else if(tmp ->info < par->info )
+    else if( tmp -> info < par -> info )
     {
-      par->left_child = tmp;
+      par -> left_child = tmp;
     }
     else
     {
-      par->right_child = tmp;
+      par -> right_child = tmp;
     }
     insert_balance(tmp);
 }
@@ -326,7 +354,7 @@ void insert_balance( struct red_black *ptr )
 void display_tree( struct red_black *ptr , int level )
 {
     int i ;
-    if ( ptr == NULL )
+    if ( ptr == leaf )
     {
         return ;
     }
@@ -344,4 +372,210 @@ void display_tree( struct red_black *ptr , int level )
             printf(ANSI_COLOR_CYAN "%d" ANSI_COLOR_CYAN , ptr -> info );
         display_tree( ptr -> left_child , level + 3 ) ;
     }
+}
+void preorder_display( struct red_black *root )
+{
+      if ( root == leaf )
+      {
+            return;
+      }
+      printf("%d ", root -> info );
+      preorder_display( root -> left_child );
+      preorder_display( root -> right_child );
+}
+void inorder_display( struct red_black *root )
+{
+      if ( root == leaf )
+      {
+            return;
+      }
+      inorder_display( root -> left_child );
+      printf("%d ", root -> info );
+      inorder_display( root -> right_child );
+}
+void postorder_display( struct red_black *root )
+{
+      if ( root == leaf )
+      {
+            return;
+      }
+      postorder_display( root -> left_child );
+      postorder_display( root -> right_child );
+      printf("%d ", root -> info );
+}
+
+int get_height( struct red_black *ptr)
+{
+   int left_height , right_height ;
+   if ( ptr == leaf )
+   {
+      return 0;
+   }
+   left_height = get_height( ptr -> left_child );
+   right_height = get_height( ptr -> right_child );
+   return ( left_height > right_height ? left_height + 1 : right_height + 1);
+}
+
+struct red_black * search( int data)
+{
+    struct red_black *ptr = root ;
+    while( ptr != leaf )
+    {
+        if ( data < ptr -> info )
+        {
+            ptr = ptr -> left_child;
+        }
+        else if ( data > ptr -> info )
+        {
+            ptr = ptr -> right_child;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return ptr ;
+}
+struct red_black *get_successor( struct red_black *ptr )
+{
+    ptr = ptr -> right_child ;
+    while (ptr -> left_child != leaf )
+    {
+        ptr = ptr -> left_child ;
+    }
+
+    return ptr ;
+}
+
+void delete( int data )
+{
+    struct red_black *ptr = search(data) , *successor , *child_ptr = NULL ;
+    if ( ptr != leaf )
+    {
+        if ( ptr -> left_child != leaf && ptr -> right_child != leaf )
+        {
+            successor = get_successor(ptr);
+            ptr -> info = successor -> info ;
+            ptr = successor ;
+        }
+        if ( ptr -> left_child != leaf )
+        {
+            child_ptr = ptr -> left_child ;
+        }
+        else
+        {
+            child_ptr = ptr -> right_child ;
+        }
+        child_ptr -> parent = ptr -> parent ;
+        if ( ptr -> parent == leaf )
+        {
+            root = child_ptr;
+        }
+        else if ( ptr == ptr -> parent -> left_child )
+        {
+            ptr -> parent -> left_child = child_ptr ;
+        }
+        else
+        {
+            ptr -> parent -> right_child = child_ptr ;
+        }
+        if ( child_ptr == root )
+        {
+            child_ptr -> color = BLACK ;
+        }
+        else if ( ptr -> color == BLACK )
+        {
+            if ( child_ptr != leaf )
+                child_ptr -> color = BLACK ;
+            else
+                delete_balance(child_ptr);
+        }
+    }
+    else
+    {
+        printf("\n%d is not present in the tree!!!",data);
+    }
+}
+
+void delete_balance(struct red_black *ptr)
+{
+  struct red_black *sib;
+
+  while( ptr != root )
+  {
+    if( ptr != NULL && ptr == ptr-> parent-> left_child )
+    {
+            sib = ptr -> parent -> right_child;
+            if( sib -> color == RED )
+            {
+                sib -> color = BLACK ;
+                ptr -> parent -> color = RED;
+                left_rotate( ptr -> parent);
+                sib = ptr -> parent -> right_child; 
+            }
+            if(sib -> left_child -> color == BLACK && sib -> right_child -> color == BLACK )
+            {
+                sib -> color = RED ;
+                if( ptr -> parent -> color == RED )
+                {
+                    ptr -> parent -> color = BLACK;
+                    return;
+                }
+                else
+                    ptr = ptr -> parent; 
+            }
+            else
+            {
+                if( sib -> right_child -> color == BLACK )  
+                {
+                    sib -> left_child -> color = BLACK;
+                    sib -> color = RED;
+                    right_rotate(sib);
+                    sib = ptr -> parent -> right_child;
+                }
+                sib -> color = ptr -> parent -> color; 
+                ptr -> parent -> color = BLACK;
+                sib -> right_child -> color = BLACK;
+                left_rotate( ptr -> parent);
+                return;
+            }
+    }
+    else
+    {
+            sib = ptr -> parent -> left_child;
+            if( sib -> color == RED )
+            {
+                sib -> color = BLACK ;
+                ptr -> parent -> color = RED;
+                right_rotate( ptr -> parent);
+                sib = ptr -> parent -> left_child; 
+            }
+            if(sib -> left_child -> color == BLACK && sib -> right_child -> color == BLACK )
+            {
+                sib -> color = RED ;
+                if( ptr -> parent -> color == RED )
+                {
+                    ptr -> parent -> color = BLACK;
+                    return;
+                }
+                else
+                    ptr = ptr -> parent;  
+            }
+            else
+            {
+                if( sib -> left_child -> color == BLACK ) 
+                {
+                    sib -> right_child -> color = BLACK;
+                    sib -> color = RED;
+                    left_rotate(sib);
+                    sib = ptr -> parent -> left_child;
+                }
+                sib -> color = ptr -> parent -> color;  
+                ptr -> parent -> color = BLACK;
+                sib -> left_child -> color = BLACK;
+                right_rotate( ptr -> parent);
+                return;
+            }
+    }
+  }
 }
